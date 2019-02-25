19594561cd727dc1c7c03626740c13613b1b3a7d
Hao Jiang <haojiang@kth.se>
2019-02-25 09:20:00 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,9 @@ DEFAULT_LANG = u'en'
 THEME = 'tuxlite_tbs'
 
 DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+USE_FOLDER_AS_CATEGORY = True
+DELETE_OUTPUT_DIRECTORY = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
