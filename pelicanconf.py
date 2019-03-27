69c352c206639e3ef29e9234b77da8e9e42b846f
Hao Jiang <haojiang@kth.se>
2019-03-27 14:21:26 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,7 +28,7 @@ EXTRA_PATH_METADATA = {
         }
 SLUGIFY_SOURCE = 'basename'
 DEFAULT_DATE = 'fs'
-DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives']
+DIRECT_TEMPLATES = ['categories', 'authors', 'archives']
 
 # 页面显示
 SUMMARY_MAX_LENGTH = 50
@@ -36,7 +36,7 @@ DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
 DEFAULT_ORPHANS = 0
 DEFAULT_PAGINATION = 10
-# PAGINATED_TEMPLATES = {'index': None, 'tag': None, 'category': None, 'author': None}
+PAGINATED_TEMPLATES = {'index': None, 'tag': None, 'category': None, 'author': None}
 NEWEST_FIRST_ARCHIVES = True
 
 # 主题/插件
