65a1621483aae03ec4c2b85521efe266525f9a49
Hao Jiang <haojiang@kth.se>
2019-03-27 15:21:33 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -70,12 +70,6 @@ ARCHIVES_URL = 'articles/index.html'
 ARCHIVES_SAVE_AS = ARCHIVES_URL
 DEFAULT_LANG = u'zh_CN'
 DEFAULT_CATEGORY = 'uncategorized'
-SLUG_REGEX_SUBSTITUTIONS = [
-(r'[^ws-]', ''), # remove non-alphabetical/whitespace/'-' chars
-(r'(?u)As*', ''), # strip leading whitespace
-(r'(?u)s*Z', ''), # strip trailing whitespace
-(r'[-s]+', '-'), # reduce multiple whitespace or '-' to single '-'
-]
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = None
