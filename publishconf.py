80099fc0f7ca69688f103e1e7f314782775dc5e1
Hao Jiang <haojiang@kth.se>
2019-03-27 16:08:12 +0800

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,15 +11,15 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://www.jiang-hao.com'
-RELATIVE_URLS = False
+# SITEURL = 'https://jiang-hao.com'
+# RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = None
+# CATEGORY_FEED_ATOM = None
 
-DELETE_OUTPUT_DIRECTORY = True
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
