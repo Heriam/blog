5d0846db943bcc90e26139fb19de58ea74f669cf
Hao Jiang <haojiang@kth.se>
2019-03-28 17:11:43 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,6 +33,7 @@ DIRECT_TEMPLATES = ['tags', 'categories', 'authors', 'archives']
 
 # Markdown扩展
 MARKDOWN = {
+	'extensions' : ['codehilite', 'extra', 'latex', 'toc'],
     'extension_configs': {
         'markdown.extensions.codehilite': {'css_class': 'highlight'},
         'markdown.extensions.extra': {},
