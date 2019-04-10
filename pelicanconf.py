5ddcc460182d01c1047fd72ab411718f70cc5589
Hao Jiang <haojiang@kth.se>
2019-04-10 14:41:22 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -86,7 +86,6 @@ SLUG_REGEX_SUBSTITUTIONS = [
         (r'(?u)\A\s*', ''),  # strip leading whitespace
         (r'(?u)\s*\Z', ''),  # strip trailing whitespace
         (r'[-\s]+', '-'),  # reduce multiple whitespace or '-' to single '-'
-		(r'\w', '-'),
 		(r' ', '-'),
     ]
 
